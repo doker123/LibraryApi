4dee2fbd48f6c57cad827c5bbe8f7d71e76bd13a
doker123 <dodo2larin@gmail.com>
2025-11-19 11:24:00 +0700

    Начал писать апи библиотеки.

---
+++ LibraryApi/urls.py
@@ -0,0 +1,11 @@
+# library_api/config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('books.urls')),  # подключаем маршруты приложения
+    path('api-auth/', include('rest_framework.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ books/admin.py
@@ -0,0 +1,28 @@
+# library_api/books/admin.py
+from django.contrib import admin
+from .models import Author, Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
+    ordering = ('name',)
+    fields = ('name', 'biography', 'birth_date')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'genre', 'publisher', 'year')
+    list_filter = ('category', 'genre', 'year', 'publisher')
+    search_fields = ('title', 'author__name', 'genre')
+    ordering = ('title', 'year')
+    fields = (
+        'title',
+        'author',
+        'year',
+        'genre',
+        'category',
+        'publisher',
+        'cover_image',
+        'file'
+    )
+    raw_id_fields = ('author',)  # удобно при большом числе авторов
